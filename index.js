efc323e92414dc2c452c38e5f0243135c5cb2c5b
Salam Mubarak A <88565304+MADES99@users.noreply.github.com>
2023-06-20 21:28:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,24 +12,12 @@ let start = 0;
 homeScore.innerText = count;
 guestScore.innerText = start;
 
-function statusDisplay() {
-    if (homeScore.innerText > guestScore.innerText){
-        statusEl.innerText = 'HOME IS WINING';
-    } else if (guestScore.innerText > homeScore.innerText) {
-        statusEl.innerText = 'GUEST IS WINING';
-    } else if (homeScore.innerText == guestScore.innerText) {
-        statusEl.innerText = 'DRAW';
-    }
-}
-
-
 resetBtn.addEventListener('click', () => {
     count = 0;
     start = 0;
     homeScore.innerText = count;
     guestScore.innerText = start;
 })
-
 
 homeEl.addEventListener('click', (e) => {
     let btnValue = +e.target.innerText;
@@ -58,4 +46,5 @@ guestEl.addEventListener('click', (e) => {
         start += 3;
       guestScore.innerText = start;
     }
+      statusDisplay();
 });
